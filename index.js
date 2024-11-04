475f2d6f94f0515bec66326d3bae825bcf593fa7
Paras Bagri <parasba85@gmail.com>
2024-11-04 16:42:13 +0530

    file pushed

---
+++ index.js
@@ -0,0 +1,8 @@
+// JavaScript function to change the text
+function changeText() {
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    
+    title.textContent = "You clicked the button!";
+    description.textContent = "The text has been changed using JavaScript.";
+}
